3cca3417b70251ffecaf73bdee5a9520ff8f2395
KonTroAl <troshenkin.k@yandex.ru>
2020-11-02 19:40:21 +0500

    completed tasks 1 and 2

--- task_2.py
+++ task_2.py
@@ -14,10 +14,30 @@
 
 Добавьте аналитику: что вы сделали и почему
 """
+import timeit
 
 
+def memorize(func):
+    def g(n, memory={}):
+        r = memory.get(n)
+        if r is None:
+            r = func(n)
+            memory[n] = r
+        return r
+
+    return g
+
+
+@memorize
 def recursive_reverse(number):
     if number == 0:
         return str(number % 10)
     return f'{str(number % 10)}{recursive_reverse(number // 10)}'
 
+
+number = 456
+
+print(timeit.timeit("recursive_reverse(number)", setup="from __main__ import recursive_reverse, number"))
+
+"""Для оптимимзации кода использовалась мемоизация, которая в случае рекурсии позволяет запоминать значения 
+и ускорять процесс вывода значений"""
